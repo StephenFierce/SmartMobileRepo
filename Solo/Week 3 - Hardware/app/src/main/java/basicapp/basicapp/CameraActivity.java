7abe228c66623c20ffb7812eb0ac05bb88507558
Stephen <stephenfiers@gmail.com>
2018-03-08 12:12:26 +0100

    POC - Hardware (Foto nemen met Camera) POC - Data persistence (Foto opslaan)

---
+++ Solo/Week 3 - Hardware/app/src/main/java/basicapp/basicapp/CameraActivity.java
@@ -0,0 +1,20 @@
+package basicapp.basicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+
+
+    public void takePicture(View v){
+
+    }
+}
--- Solo/Week 3 - Hardware/app/src/main/java/basicapp/basicapp/MainActivity.java
+++ Solo/Week 3 - Hardware/app/src/main/java/basicapp/basicapp/MainActivity.java
@@ -1,12 +1,23 @@
 package basicapp.basicapp;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,10 +29,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -104,5 +122,59 @@ public class MainActivity extends AppCompatActivity
     public void loadRegister(View view){
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void loadCamera(View view) {
+        File photoFile = null;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(Build.VERSION.SDK_INT>=24){
+                try{
+                    Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                    m.invoke(null);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+//            Uri imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"fname_" +
+//                    String.valueOf(System.currentTimeMillis()) + ".jpg"));
+//            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
+//            startActivityForResult(intent, 0);
+
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex){
+
+            }
+            if (photoFile != null){
+                Uri photoUri = FileProvider.getUriForFile(this,"com.example.android.fileprovider",photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent,0);
+            }
+        }
     }
 }
